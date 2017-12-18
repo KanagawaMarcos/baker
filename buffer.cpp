f863c0c7e9defbcd09e37ef191600d9c4f27b1a3
Marcos Costa <mcs1901@outlook.com>
2017-12-18 17:50:01 -0300

    Criado um Makefile ultimate simples

--- buffer.cpp
+++ buffer.cpp
@@ -9,18 +9,14 @@ character* createChar(char text){
   //Create a new character
   character* newCharacter = NULL;
 
-  //Return NULL if the text is actually empty
-  if(strcmp(text, '')){
+  //Allocate a new character
+  newCharacter = new character[1];
 
-    //Allocate a new character
-    newCharacter = new newCharacter[1];
-
-    //Fill the new character with the right data
-    if(newCharacter != NULL){
-      newCharacter->next = NULL;
-      newCharacter->previous = NULL;
-      newCharacter->text = text;
-    }
+  //Fill the new character with the right data
+  if(newCharacter != NULL){
+    newCharacter->next = NULL;
+    newCharacter->previous = NULL;
+    newCharacter->text = text;
   }
 
   //Return the newCharacter or NULL if it was not allocated
@@ -28,5 +24,6 @@ character* createChar(char text){
 }
 
 int destroyString(character** string){
-  return errorCode = 0;
+  int errorCode = 0;
+  return errorCode;
 }
