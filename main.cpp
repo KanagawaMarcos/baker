b57370451c4d1b9f7580cbb1bda4aa21141a5dfb
Marcos Costa <mcs1901@outlook.com>
2017-12-18 16:34:11 -0300

    adicionado arquivos de lista do geraldo

--- main.cpp
+++ main.cpp
@@ -1,50 +1,20 @@
+#include "lista.h"
 #include <iostream>
-#include <fstream>
-#include <string.h>
 
 using namespace std;
 
-int qtdPalavrasNoArq(const char* nomeArquivo);
-int tamMaiorPalavra(const char* nomeArquivo);
+int main() {
+    //cab = cabeça
+    lista *cab=NULL;
+    int dado = 1;
 
-int main (int argv, char* argc[]){
-    const char* nomeArquivo = "teste.txt";
-
-    int numPalavras = qtdPalavrasNoArq(nomeArquivo);
-    int maiorTam = tamMaiorPalavra(nomeArquivo);
-
-
-
-    return 0;
-}
-
-int qtdPalavrasNoArq(const char* nomeArquivo){
-  ifstream arquivo;
-  arquivo.open(nomeArquivo);
-  char nomeAtual[257];
-  int numeroDePalavras = 0;
-
-  while(arquivo >> nomeAtual){
-        numeroDePalavras++;
-  }
-
-  arquivo.close();
-  return numeroDePalavras;
-}
-
-int tamMaiorPalavra(const char* nomeArquivo){
-  int maiorTamanho = 0;
-  char nomeAtual[257];
-
-  ifstream arquivo;
-  arquivo.open(nomeArquivo);
-
-  while(arquivo >> nomeAtual){
-    if(maiorTamanho < strlen(nomeAtual)){
-      maiorTamanho = strlen(nomeAtual);
+    while (dado > 0) {
+        cin >> dado;
+        inserirInicio(&cab, dado);
+        imprimir(cab);
     }
-  }
 
-  arquivo.close();
-  return (maiorTamanho - 1);
+    desalocar(&cab);
+    cout << "Desespero :";
+    imprimir(cab);
 }
