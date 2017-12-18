aec0b5c5910b36be1b8cff63cd72e4318813b933
Marcos Costa <mcs1901@outlook.com>
2017-12-18 17:14:13 -0300

    Criado cabeçalho do meu buffer e da função de criar letra

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
+#include "buffer.h"
 #include <iostream>
 #include <string.h>
 
 using namespace std;
 
 int main (int argv, char* argc[]){
-    
+    character* letra = createChar('a');
+    cout << letra->text << endl;
     return 0;
 }
