f755974c8fa2ade23c97c07658c6ce6af07fef02
Marcos Costa Santos <mcs1901@outlook.com>
2017-12-31 15:40:07 -0300

    "Regra" data structure removed

--- main.cpp
+++ main.cpp
@@ -8,15 +8,10 @@
 using namespace std;
 
 int main (int argv, char* argc[]){
-    char* qualis = new char[3];
-    qualis[0] = 'B';
-    qualis[1] = '5';
-    qualis[2] = '\0';
-
 
     //Create a double linked list of all "docentes"
     docente* docentes = loadAllDocentes("csv/docentes.csv");
-    regra* tmp = createRegra(qualis,25);
+
 
     //Create a binary search tree of all "periodicos"
     //periodico* periodicos = loadAllPeriodicos("csv/qualis_capes_periodicos.csv");
