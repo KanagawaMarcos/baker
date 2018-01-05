c8f186fa5d4b7e0ed684cc04633b999334f1646c
Marcos Costa <mcs1901@outlook.com>
2018-01-05 10:12:54 -0300

    arquivo correto de orientação carregado

--- main.cpp
+++ main.cpp
@@ -15,14 +15,14 @@ int main (int argv, char* argc[]){
     int* rules = loadAllRules("csv/regras/regraComp",22);
 
     //Buffer files contain all valid "capes" and "orientações"
-    character* orientacoes = createBufferFile("csv/qualis_capes_congressos.csv");
+    character* orientacoes = createBufferFile("csv/orientacao.csv");
     character* congressos = createBufferFile("csv/qualis_capes_congressos.csv");
     character* periodicos = createBufferFile("csv/qualis_capes_periodicos.csv");
 
     //Will fill all give a pontuation to all variables of type "docente"
     baker(&docentes, &producoes, rules, orientacoes, congressos, periodicos);
 
-    
+
     destroyAllProducoes(&producoes);
     destroyAllDocentes(&docentes);
 
