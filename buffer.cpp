a9687af74f0d96035b1cb8a704a8d4dec0ba0c48
Marcos Costa Santos <mcs1901@outlook.com>
2018-01-07 14:47:47 -0300

    ignore the commit before this one should be a bug fix, not a bug report

--- buffer.cpp
+++ buffer.cpp
@@ -500,6 +500,7 @@ char* getNthColumnLocalOrientacao(character* bufferFile, int collumn){
   return data;
 }
 
+
 char* getNthColumnLocal(character* bufferFile){
   char* string = NULL;
   if(bufferFile != NULL){
