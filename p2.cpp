88de52615d32aa58725ad57cb0431f8652ffda50
Marcos Costa <mcs1901@outlook.com>
2018-01-09 13:29:32 -0300

    primeira parcial e segunda questão da p2

---
+++ p2.cpp
@@ -0,0 +1,255 @@
+#include <iostream>
+#include <stdlib.h>
+
+
+using namespace std;
+
+//char* primeira(char**trajetoria, int rows, int collumns, int i, int j);
+int primeira(char**trajetoria, int rows, int collumns);
+//char** segunda(char*** trajetorias, int* linhas, int* colunas, int numeroTrajetorias);
+
+int main (int argv, char* argc[]){
+
+  //char trajetoria[8][10] = {'5','5','4','0','0','0','0','0','0','0','0','0','0','4','0','0','0','0','0','0','0','0','0','0','4','4','0','0','0','0','0','0','0','0','0','0',3,'0','0','0','0','0','0','0','0','0','0',3,3,'0','0','0','0','0','0','0','0','0',3,'0','0','0','0','0','0','0','0','0',3,'0','0','0','0','0','0','0','0','0',3,'4'};
+
+  //char trajetoria[2][3] = {'1','1','1','0','0','0'};
+  char** trajetoria = new char*[2];
+  for(int i=0; i<2; i++)
+    trajetoria[i] = new char[3];
+
+  trajetoria[0][0] = '1'; trajetoria[0][1] = '1'; trajetoria[0][2] = '1';
+  trajetoria[1][0] = '0'; trajetoria[1][1] = '0'; trajetoria[1][2] = '0';
+
+
+  cout << "return = " << primeira(trajetoria, 2, 3) << endl;
+  char* trajetoriaReduzida; //3:0 1:7 2:0 1:7 2:0 3:6 1:0
+  //trajetoriaReduzida = primeira(trajetoria, 8,10, 0, 0);
+  return 0;
+  /*
+  *   3   2   1
+  *   4       0
+  *   5   6   7
+  */
+}
+
+int primeira(char**trajetoria, int rows, int collumns){
+  int* pilhaI = new int[rows*collumns];
+  int* pilhaJ = new int[rows*collumns];
+  int top = 0;
+
+  //Itera pela matriz trajetoria criando a pilha do caminho feito
+  for( pilhaI[0] = 0; pilhaI[top] < rows; ){
+    for( pilhaJ[0] = 0; pilhaJ[top] < collumns; ){
+
+      //Tenta mover para frente (PUSH)
+      for(int dir=0; dir<7; dir++){
+        switch (dir) {
+          //Testa para direita
+          case 0:{
+
+            //Se é possível mover para direita
+            if((pilhaJ[top]+1) < collumns && trajetoria[pilhaI[top]][(pilhaJ[top]+1)] != '0' ){
+
+              //Push nova posicao
+              top++;
+              pilhaI[top] = pilhaI[(top-1)];
+              pilhaJ[top] = pilhaJ[(top-1)] + 1;
+
+              cout << "direita" << endl;
+              cout << "saindo da posicao i=" << pilhaI[(top-1)] << " j=" << pilhaJ[(top-1)] << " para posicao i=" << pilhaI[top] << " j=" << pilhaJ[top] << endl;
+              cout << "======================" << endl;
+              //Reseta a seta de verificação de posicao
+              dir = 0;
+            }
+            break;
+          }
+          //Testa para diagonal direita superior
+          case 1:{
+
+            //Se é possível mover para diagonal direita superior
+            if((pilhaJ[top]+1) < collumns && (pilhaI[top]-1) > -1 && trajetoria[(pilhaI[top]-1)][(pilhaJ[top]+1)] != '0' ){
+
+              //Push nova posicao
+              top++;
+              pilhaI[top] = pilhaI[(top-1)] - 1;
+              pilhaJ[top] = pilhaJ[(top-1)] + 1;
+
+              cout << "diagonal direita superior" << endl;
+              cout << "saindo da posicao i=" << pilhaI[(top-1)] << " j=" << pilhaJ[(top-1)] << " para posicao i=" << pilhaI[top] << " j=" << pilhaJ[top] << endl;
+              cout << "======================" << endl;
+              //Reseta a seta de verificação de posicao
+              dir = 0;
+            }
+            break;
+          }
+          //Testa para cima
+          case 2:{
+
+            //Se é possível mover para cima
+            if((pilhaI[top]-1) > -1 && trajetoria[(pilhaI[top]-1)][(pilhaJ[top])] != '0'){
+
+              //Push nova posicao
+              top++;
+              pilhaI[top] = pilhaI[(top-1)] - 1;
+              pilhaJ[top] = pilhaJ[(top-1)];
+
+              cout << "cima" << endl;
+              cout << "saindo da posicao i=" << pilhaI[(top-1)] << " j=" << pilhaJ[(top-1)] << " para posicao i=" << pilhaI[top] << " j=" << pilhaJ[top] << endl;
+              cout << "======================" << endl;
+              //Reseta a seta de verificação de posicao
+              dir = 0;
+            }
+            break;
+          }
+          //Testa para diagonal esquerda superior
+          case 3:{
+
+            //Se é possível mover para diagonal esquerda superior
+            if((pilhaJ[top]-1) > -1 && (pilhaI[top]-1) > -1 && trajetoria[(pilhaI[top]-1)][(pilhaJ[top]-1)] != '0'){
+
+              //Push nova posicao
+              top++;
+              pilhaI[top] = pilhaI[(top-1)] - 1;
+              pilhaJ[top] = pilhaJ[(top-1)] - 1;
+
+              cout << "diagonal esquerda superior" << endl;
+              cout << "saindo da posicao i=" << pilhaI[(top-1)] << " j=" << pilhaJ[(top-1)] << " para posicao i=" << pilhaI[top] << " j=" << pilhaJ[top] << endl;
+              cout << "======================" << endl;
+              //Reseta a seta de verificação de posicao
+              dir = 0;
+            }
+            break;
+          }
+          //Testa para esquerda
+          case 4:{
+
+            //Se é possível mover para esquerda
+            if((pilhaJ[top]-1) > -1 && trajetoria[pilhaI[top]][(pilhaJ[top]-1)] != '0' && (pilhaJ[top-1]) != (pilhaJ[top]-1) ){
+
+              //Push nova posicao
+              top++;
+              pilhaI[top] = pilhaI[(top-1)];
+              pilhaJ[top] = pilhaJ[(top-1)] - 1;
+
+              cout << "esquerda" << endl;
+              cout << "saindo da posicao i=" << pilhaI[(top-1)] << " j=" << pilhaJ[(top-1)] << " para posicao i=" << pilhaI[top] << " j=" << pilhaJ[top] << endl;
+              cout << "======================" << endl;
+              //Reseta a seta de verificação de posicao
+              dir = 0;
+            }
+            break;
+          }
+          //Testa para diagonal esquerda inferior
+          case 5:{
+
+            //Se é possível mover para diagonal esquerda inferior
+            if((pilhaJ[top]-1) > -1 && (pilhaI[top]+1) < rows && trajetoria[(pilhaI[top]+1)][(pilhaJ[top]-1)] != '0'){
+
+              //Push nova posicao
+              top++;
+              pilhaI[top] = pilhaI[(top-1)] + 1;
+              pilhaJ[top] = pilhaJ[(top-1)] - 1;
+
+              cout << "diagonal esquerda inferior" << endl;
+              cout << "saindo da posicao i=" << pilhaI[(top-1)] << " j=" << pilhaJ[(top-1)] << " para posicao i=" << pilhaI[top] << " j=" << pilhaJ[top] << endl;
+              cout << "======================" << endl;
+              //Reseta a seta de verificação de posicao
+              dir = 0;
+            }
+            break;
+          }
+          //Testa para baixo
+          case 6:{
+
+            //Se é possível mover para baixo
+            if((pilhaI[top]+1) < rows && trajetoria[(pilhaI[top]+1)][(pilhaJ[top])] != '0'){
+
+              //Push nova posicao
+              top++;
+              pilhaI[top] = pilhaI[(top-1)] + 1;
+              pilhaJ[top] = pilhaJ[(top-1)];
+
+              cout << "baixo" << endl;
+              cout << "saindo da posicao i=" << pilhaI[(top-1)] << " j=" << pilhaJ[(top-1)] << " para posicao i=" << pilhaI[top] << " j=" << pilhaJ[top] << endl;
+              cout << "======================" << endl;
+              //Reseta a seta de verificação de posicao
+              dir = 0;
+            }
+            break;
+          }
+          //Testa para diagonal direita inferior
+          case 7:{
+            //Se é possível mover para diagonal direita inferior
+            if((pilhaJ[top]+1) < collumns && (pilhaI[top]+1) < rows && trajetoria[(pilhaI[top]+1)][(pilhaJ[top]+1)] != '0'){
+
+              //Push nova posicao
+              top++;
+              pilhaI[top] = pilhaI[(top-1)] + 1;
+              pilhaJ[top] = pilhaJ[(top-1)] + 1;
+
+              cout << "diagonal direita inferior" << endl;
+              cout << "saindo da posicao i=" << pilhaI[(top-1)] << " j=" << pilhaJ[(top-1)] << " para posicao i=" << pilhaI[top] << " j=" << pilhaJ[top] << endl;
+              cout << "======================" << endl;
+              //Reseta a seta de verificação de posicao
+              dir = 0;
+            }
+            break;
+          }
+        }
+      }
+
+      //Se achar o final da trajetoria
+      if(pilhaI[top] < rows && pilhaJ[top] < collumns)
+        break;
+
+      //Move para trás (POP)
+      top--;
+      cout << "pop, newTop=" << top <<endl;
+
+    }
+  }
+  return 1;
+}
+/*
+char** segunda(char*** trajetorias, int* linhas, int* colunas, int numeroTrajetorias){
+  char** trajetoriaRes = NULL;
+  int maiorNumLinhas = 0;
+  int maiorNumColunas = 0;
+
+  //Calcula as dimensões da trajetoria resultante
+  for(int i=0; i<numeroTrajetorias; i++){
+
+    if(linhas[i] > maiorNumLinhas)
+      maiorNumLinhas = linhas[i];
+
+    if(colunas[i] > maiorNumColunas)
+      maiorNumColunas = colunas[i];
+  }
+
+  //Aloca a trajetoria resultante
+  trajetoriaRes = new char*[maiorNumLinhas];
+  for(int i=0; i<maiorNumLinhas; i++)
+    trajetoriaRes[i] = new char[maiorNumColunas];
+
+  //Limpa os valores da matriz resultante
+  for(int i=0; i<maiorNumLinhas; i++){
+    for(int j=0; j<maiorNumColunas; j++){
+      trajetoriaRes[i][j] = '0';
+    }
+  }
+
+  //Itera pelas trajetorias somando elas
+  for(int n=0; n < numeroTrajetorias; n++){
+
+    //Soma a matriz atual à resultante
+    for(int i=0; i<linhas[n]; i++){
+      for(int j=0; j<colunas[n]; j++){
+        trajetoriaRes[i][j] = '0' + atoi(trajetoriaRes[i][j]) + atoi(trajetorias[n][i][j]);
+      }
+    }
+  }
+
+  return trajetoriaRes;
+}
+
+*/
